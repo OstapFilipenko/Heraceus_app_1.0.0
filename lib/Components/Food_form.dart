16139ce2f9fa386489c13e9b9bbeb8b57aeca931
OstapFilipenko_mea <ostap.filipenko@mea-it.services>
2020-03-22 21:01:51 +0100

    added the validator for the submit button

--- lib/Components/Food_form.dart
+++ lib/Components/Food_form.dart
@@ -6,46 +6,65 @@ class FoodForm extends StatefulWidget {
 }
 
 class _FoodFormState extends State<FoodForm> {
+ final _formKey = GlobalKey<FormState>();  
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        TextFormField(  
-            decoration: const InputDecoration(  
-              icon: const Icon(Icons.local_dining),  
-              hintText: 'Enter the new Food',  
-              labelText: 'Food',  
-            ),  
-          ),  
+    return Form(
+      key: _formKey,
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
           TextFormField(  
-            decoration: const InputDecoration(  
-              icon: const Icon(Icons.loop),  
-              hintText: 'Enter the colorine',  
-              labelText: 'Calorine',  
+              decoration: const InputDecoration(  
+                icon: const Icon(Icons.local_dining),  
+                hintText: 'Enter the new Food',  
+                labelText: 'Food',  
+              ),
+              validator: (value){
+                if(value.isEmpty){
+                  return 'Please enter a Value';
+                }
+              },  
             ),  
-          ), 
-          Center(
-            child: FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              /**
-               * Die Daten schicken
-               */
-            },
-            child: Text(
-              "Add",
-              style: TextStyle(fontSize: 20.0),
+            TextFormField(  
+              decoration: const InputDecoration(  
+                icon: const Icon(Icons.loop),  
+                hintText: 'Enter the colorine',  
+                labelText: 'Calorine',  
+              ),
+              validator: (value){
+                if(value.isEmpty){
+                  return 'Please enter a Value';
+                }
+              },  
+            ), 
+            Center(
+              child: FlatButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              disabledColor: Colors.grey,
+              disabledTextColor: Colors.black,
+              padding: EdgeInsets.all(8.0),
+              splashColor: Colors.blueAccent,
+              onPressed: () {
+                if (_formKey.currentState.validate()) {  
+                    // If the form is valid, display a Snackbar.  
+                    Scaffold.of(context)  
+                        .showSnackBar(SnackBar(content: Text('Adding the Datas into the DB...')));  
+                  }  
+              },
+              child: Text(
+                "Add",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ) 
             ),
-          ) 
-          ),
-           
-      ],
-    );
+            
+        ],
+      )
+    ); 
+    
+   
   }
 }
